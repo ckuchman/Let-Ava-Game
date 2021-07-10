d1b9944a9b46df1927b5a0e629ee99e9e80d7726
kuch517 <kuch517@gmail.com>
2021-07-10 08:46:11 -0700

    paperplanes tracking

--- Let Ava Game/Assets/Scripts/Spawner.cs
+++ Let Ava Game/Assets/Scripts/Spawner.cs
@@ -7,31 +7,30 @@ public class Spawner : MonoBehaviour
     public GameObject obstacle_ground;
     public GameObject obstacle_air;
 
-    private float timeBtwSpawn;
+    private float _timeBtwSpawn;
     public float minTimeBtwSpawn;
     public float maxTimeBtwSpawn;
+    private Vector3 _startPosition;
+
+
+    void Start () {
+        _startPosition = transform.position;
+    }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (timeBtwSpawn <= 0)
-        {
-            if (Random.value < 0.3) 
-            {
+    void Update() {
+        if (_timeBtwSpawn <= 0) {
+            if (Random.value < 0.3) {
                 float rand_height = Random.Range(3, 6);
-                Instantiate(obstacle_air, new Vector3(transform.position.x, transform.position.y + rand_height, transform.position.x), Quaternion.identity);
-            }
-            else
-            {
+                Instantiate(obstacle_air, _startPosition + new Vector3(0.0f, rand_height, 0.0f), Quaternion.identity);
+            } else {
                 Instantiate(obstacle_ground, transform.position, Quaternion.identity);
             }
             
             // Randomly set the time for the next obstacle
-            timeBtwSpawn = Random.Range(minTimeBtwSpawn, maxTimeBtwSpawn);
-        }
-        else
-        {
-            timeBtwSpawn -= Time.deltaTime;
+            _timeBtwSpawn = Random.Range(minTimeBtwSpawn, maxTimeBtwSpawn);
+        } else {
+            _timeBtwSpawn -= Time.deltaTime;
         }
     }
 }
