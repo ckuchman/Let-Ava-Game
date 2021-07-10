5829c7920c49972691f91f5edf1ab65281835759
kuch517 <kuch517@gmail.com>
2021-07-09 20:13:50 -0700

    added basic obstacle

---
+++ Let Ava Game/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) {
+            //other.GetComponent<Player>().health -= damage;
+            Destroy(gameObject);
+        }
+    }
+}
