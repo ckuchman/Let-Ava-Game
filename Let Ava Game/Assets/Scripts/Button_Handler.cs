f79cc50040ac8b5b42b410f2d92ab254f367733c
Chris <kuch517@gmail.com>
2021-07-10 11:18:31 -0700

    Started Pause menu

--- Let Ava Game/Assets/Scripts/Button_Handler.cs
+++ Let Ava Game/Assets/Scripts/Button_Handler.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;  
 
 public class Button_Handler : MonoBehaviour
 {    
-    public void doExitGame() {
+    public void exitGame() {
         Application.Quit();
     }
+
+    public void startGame() {  
+        SceneManager.LoadScene("Main");  
+    }  
 }
